ddd3fbbc5963ba0f9e98eb392d434d3367dd14ee
Jorge <jorgeantoniobarrios@gmail.com>
2025-07-03 20:55:44 -0500

    appointments

--- db/queries/appoinments.js
+++ db/queries/appoinments.js
@@ -1,12 +1,54 @@
-import db from "../client.js"
+import db from "../client.js";
 
-//CREATE APPOINTMENT
-export async function createAppointment({user_id, vet_id, time, appointment_reason}){
-    const sql=`
-    INSERT INTO appointments(user_id, vet_id, time, appointment_reason)
+// Create a new appointment
+export async function createAppointment({
+  user_id,
+  vet_id,
+  time,
+  appointment_reason,
+}) {
+  const sql = `
+    INSERT INTO appointments (user_id, vet_id, time, appointment_reason)
     VALUES ($1, $2, $3, $4)
     RETURNING *
-    `;
-    const {rows:appointment} = await db.query(sql, [user_id, vet_id, time, appointment_reason]);
-    return appointment[0]
+  `;
+  const { rows: appointment } = await db.query(sql, [
+    user_id,
+    vet_id,
+    time,
+    appointment_reason,
+  ]);
+  return appointment[0];
+}
+
+// Get all appointments
+export async function getAllAppointments() {
+  const sql = `
+    SELECT * FROM appointments
+    ORDER BY time
+  `;
+  const { rows } = await db.query(sql);
+  return rows;
+}
+
+// Get appointments for user
+export async function getAppointmentsByUser(user_id) {
+  const sql = `
+    SELECT * FROM appointments
+    WHERE user_id = $1
+    ORDER BY time
+  `;
+  const { rows } = await db.query(sql, [user_id]);
+  return rows;
+}
+
+// Get appointments for vet
+export async function getAppointmentsByVet(vet_id, user_id) {
+  const sql = `
+    SELECT * FROM appointments
+    WHERE vet_id = $1 AND $2
+    ORDER BY time
+  `;
+  const { rows } = await db.query(sql, [vet_id]);
+  return rows;
 }
